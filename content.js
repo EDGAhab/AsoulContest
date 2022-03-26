d3bc80734abdc074d0c3edd7d2c998a514b87e60
Feilian Huang <fhuang49@gatech.edu>
2022-03-26 16:29:35 -0400

    bella!

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ function onload() {
       };
     pet.style.left = (position.x = 100) + 'px';
     pet.style.top = (position.y = 100) + 'px';
-    pet.classList.add('carol-pet');
+    pet.classList.add('bella-pet');
     document.body.appendChild(pet);
 }
 window.addEventListener('load', onload);
