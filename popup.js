84dbe38faafc291b06422b8bca70a801af737ae3
Shiyao-monster <shiyaochn@gmail.com>
2022-03-27 23:02:55 -0400

    update pop

--- popup.js
+++ popup.js
@@ -1,21 +1,94 @@
+
 const msg = document.querySelector('.msg');
-const changeButton = document.getElementById("pet-change");
+var changeButton1 = document.getElementById("pet-change1");
+var changeButton2 = document.getElementById("pet-change2");
+var changeButton3 = document.getElementById("pet-change3");
+var changeButton4 = document.getElementById("pet-change4");
+var changeButton5 = document.getElementById("pet-change5");
 
-changeButton.addEventListener('click', onChange);
+changeButton1.addEventListener('click', onChange1);
+changeButton2.addEventListener('click', onChange2);
+changeButton3.addEventListener('click', onChange3);
+changeButton4.addEventListener('click', onChange4);
+changeButton5.addEventListener('click', onChange5);
 
-function onChange(e) {
+
+function onChange1(e) {
     e.preventDefault();
     
-    var newContent =`<input class="btn" type="submit" id="pet-change" value="Change Pet">` +
-                     "<div class='blockContent'>" +
-                    `<div class="msg">`+ 
-                    `<h3>Carol</h3>` + 
+    petName = "Ava"
+    console.log(newPetName);
+    let image = "/images/" + petName + "Pet128.png";
+    var newContent = `<h3 id="pet-name"> ${petName} </h3>` +
                     `<p>`+
-                    `<img height="100" style="max-width: 120px;background-position: top left;" src="/images/CarolPet128.png" />`+
-                    `</p>`+
-                    `</div>`+`</div>`;
-    
-document.getElementById("my-form").innerHTML = newContent;
-
+                    `<img height="100" style="max-width: 120px;background-position: top left;" src=${image} />`+
+                    `</p>`;
+    msg.innerHTML = newContent;
 }
+
+
+function onChange1(e) {
+    e.preventDefault();
+    
+    petName = "Ava";
+    let image = "/images/" + petName + "Pet128.png";
+    var newContent = `<h3 id="pet-name"> ${petName} </h3>` +
+                    `<p>`+
+                    `<img height="100" style="max-width: 120px;background-position: top left;" src=${image} />`+
+                    `</p>`;
+    msg.innerHTML = newContent;
+}
+
+
+function onChange2(e) {
+    e.preventDefault();
+    
+    petName = "Bella";
+    let image = "/images/" + petName + "Pet128.png";
+    var newContent = `<h3 id="pet-name"> ${petName} </h3>` +
+                    `<p>`+
+                    `<img height="100" style="max-width: 120px;background-position: top left;" src=${image} />`+
+                    `</p>`;
+    msg.innerHTML = newContent;
+}
+
+
+function onChange3(e) {
+    e.preventDefault();
+    
+    petName = "Carol";
+    let image = "/images/" + petName + "Pet128.png";
+    var newContent = `<h3 id="pet-name"> ${petName} </h3>` +
+                    `<p>`+
+                    `<img height="100" style="max-width: 120px;background-position: top left;" src=${image} />`+
+                    `</p>`;
+    msg.innerHTML = newContent;
+}
+
+
+function onChange4(e) {
+    e.preventDefault();
+    
+    petName = "Diana";
+    let image = "/images/" + petName + "Pet128.png";
+    var newContent = `<h3 id="pet-name"> ${petName} </h3>` +
+                    `<p>`+
+                    `<img height="100" style="max-width: 120px;background-position: top left;" src=${image} />`+
+                    `</p>`;
+    msg.innerHTML = newContent;
+}
+
+
+function onChange5(e) {
+    e.preventDefault();
+    
+    petName = "Eileen"
+    let image = "/images/" + petName + "Pet128.png";
+    var newContent = `<h3 id="pet-name"> ${petName} </h3>` +
+                    `<p>`+
+                    `<img height="100" style="max-width: 120px;background-position: top left;" src=${image} />`+
+                    `</p>`;
+    msg.innerHTML = newContent;
+}
+
 
