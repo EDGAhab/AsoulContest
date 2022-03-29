006245171be0d003ddc3e3813326901166e0dd6d
Yao <1360686461@qq.com>
2022-03-29 14:44:57 -0400

    fixed bug [pet move when scroll down the page]

--- pet.js
+++ pet.js
@@ -3,14 +3,14 @@
 $(document).ready(function () {
     // error: $ is not defined. Pls ignore it cause this code can work.
     var pet = $("<div class='pet'></div>");
-    $("body").parent().append(pet);
+    $("body").append(pet);
     petImgURL = chrome.runtime.getURL("images/DianaStandL.png");
     $('.pet').prepend($('<img>', { src: petImgURL }));
     $(".pet").css({
         "left": "100px",
-        "bottom": "100px",
+        "top": "100px",
         "z-index": "9999",
-        "position": "absolute",
+        "position": "fixed",
     });
 
     $(".pet").draggable(); // Drag pet around
