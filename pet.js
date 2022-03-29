1a62731c2698a536499a6205ba54dbe120b6483a
Yao <1360686461@qq.com>
2022-03-29 15:03:40 -0400

    I hate .DS_Store

--- pet.js
+++ pet.js
@@ -3,12 +3,12 @@
 $(document).ready(function () {
     // error: $ is not defined. Pls ignore it cause this code can work.
     var pet = $("<div class='pet'></div>");
-    $("body").append(pet);
+    $("body").parent().append(pet);
     petImgURL = chrome.runtime.getURL("images/DianaStandL.png");
     $('.pet').prepend($('<img>', { src: petImgURL }));
     $(".pet").css({
         "left": "100px",
-        "top": "100px",
+        "bottom": "0px",
         "z-index": "9999",
         "position": "fixed",
     });
